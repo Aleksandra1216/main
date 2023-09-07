862386853dbd0955154a87d1308a5651b139e298
Aleksandra1216 <simsani@yandex.ru>
2023-09-07 21:09:12 +0300

    proverca

--- script.js
+++ script.js
@@ -1,77 +1,106 @@
 // Задание 1
-  function Number(num1, num2) {
-    if (num1 <= num2) {
-    return num1;
-    } else {
-    return num2;
-    }
-    }
-    
-    console.log(Number(8, 4)); // 4
-    console.log(Number(6, 6)); // 6
+let q = Number(prompt('Введите число'));
+let w = Number(prompt('Введите число'));
+   if (q < w) {
+    console.log(q);
+   } else {
+    console.log(w);
+   }
 
 // Задание 2
-function checkEvenOdd(number) {
-  if (number % 2 === 0) {
-  return 'Число четное';
+let d = Number(prompt('Введите число'));
+ if (d % 2 === 0) {
+  console.log("Число четное");
   } else {
-  return 'Число нечетное';
+     console.log("Число нечетное");
   }
-  }
-// Примеры использования:
-  console.log(checkEvenOdd(4)); // Число четное
-  console.log(checkEvenOdd(7)); // Число нечетное
-
-
+ 
 // Задание 3
-function squareNumber(num) {
-  var square = num * num;
-  console.log(square);
-}
-
-
-function squareNumber(num) {
-  var square = num * num;
-  return square;
-}
-
-console.log(squareNumber(7));
+let n = (prompt('Введите число'));
+console.log(`${n*n}`);
+console.log(n);
 
 // Задание 4
 let age = parseInt(prompt("Сколько вам лет?"));
-
 if (age < 0) {
   console.log("Вы ввели неправильное значение");
 } else if (age <= 12) {
-  console.log("Привет, друг!");
+ console.log("Привет, друг!");
 } else {
-  console.log("Добро пожаловать!");
+ console.log("Добро пожаловать!");
 }
 
 // Задание 5
-
-function multiplyNumbers(num1, num2) {
-let num1 =prompt("Введите первое число");
-let num2 =prompt("Введите второе число");
+let num1=Number(prompt('Введите число'));
+let num2=Number(prompt('Введите число'));
   // Проверяем, являются ли параметры числами
- /if (isNaN(num1) || isNaN(num2)) {
-/ return "Одно или оба значения не являются числом";
-  }
-  // Если оба параметра - числа, возвращаем их произведение
-  return num1 * num2;
+  if (isNaN(num1) || isNaN(num2)) {
+    console.log("Одно или оба значения не являются числом");
+  } else{
+    console.log (`${num1*num2}`);
   }
 
+ 
 // Задание 6
-
-function cubeNumber() {
   let number = prompt("Введите число:");
-  if (isNaN(number)) {
-  return "Переданный параметр не является числом";
+  if (isNaN(number)){
+    console.log("Переданный параметр не является числом");
   } else {
-  let cube = parseInt(number) ** 3;
-  return `${number} в кубе равняется ${cube}`;
-  }
+    let cube = parseInt(number) ** 3;
+    console.log(`${number} в кубе равняется ${cube}`);
   }
 
+  
 // Задание 7
+// Создание объекта circle1
+let circle1 = {
+  radius: 5, // установка свойства radius
+  
+  // Метод, возвращающий площадь круга
+  getArea: function() {
+    return Math.PI * this.radius * this.radius;
+  },
+  
+  // Метод, возвращающий периметр окружности
+  getPerimeter: function() {
+    return 2 * Math.PI * this.radius;
+  }
+};
+
+// Создание объекта circle2
+let circle2 = {
+  radius: 10, // установка свойства radius
+  
+  // Метод, возвращающий площадь круга
+  getArea: function() {
+    return Math.PI * this.radius * this.radius;
+  },
+  
+  // Метод, возвращающий периметр окружности
+  getPerimeter: function() {
+    return 2 * Math.PI * this.radius;
+  }
+};
+
+// Вызов методов объекта circle1 для получения площади и периметра
+console.log(circle1.getArea()); // Вывод: 78.53981633974483
+console.log(circle1.getPerimeter()); // Вывод: 31.41592653589793
+
+// Вызов методов объекта circle2 для получения площади и периметра
+console.log(circle2.getArea()); // Вывод: 314.1592653589793
+console.log(circle2.getPerimeter()); // Вывод: 62.8318530717958
+
+
 // Задание 8
+let month= (prompt('Введите число месяца'));
+  if (month === 12 && month >=1 && month <= 2) {
+  console.log("Зима"); 
+  } else if (month >= 3  && month <= 5) {
+  console.log("Весна") ;
+  } else if (month >= 6 &&  month <= 8) {
+  console.log("Лето"); 
+  } else if (month >=9 && month <= 11) {
+  console.log("Осень"); 
+  } else  {
+  console.log("Неправильный параметр!"); 
+ }
