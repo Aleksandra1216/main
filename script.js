d994c411f1a49200a380e8877112918714eddd40
Aleksandra1216 <simsani@yandex.ru>
2023-09-10 12:20:10 +0300

    proverka

--- script.js
+++ script.js
@@ -57,8 +57,46 @@ if (b.includes(Number(a))) {
 }
 
 // Задание 8
+const num = ['a','b','c','d','e','f'];
+num.reverse();
+console.log(num);
+
+
 // Задание 9
+let task=[
+    [1, 2, 3,],
+    [, 4, 5, 6],
+];
+task= task.join('');
+console.log(task);
+
 // Задание 10
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 6];
+
+for(let i = 0; i < arr.length - 1; i++) {
+let sum = arr[i] + arr[i + 1];
+console.log(sum);
+}
+
 // Задание 11
+function getWord(arr) {
+    return arr.map(word => (word ** 2));
+  }
+const salary = [2, 5, 7, 3, 1];
+console.log (getWord(salary));
+
 // Задание 12
+function getWordLengths(arr) {
+    return arr.map(word => word.length);
+  }
+  
+const sal = ['слово', 'цифра', 'слог', 'длинное предложение', 'буква'];
+console.log(getWordLengths(sal)); //5,5,4,18,5
+
 // Задание 13
+function filterPositive(array) {
+    return array.filter(num => num < 0);
+  }
+
+console.log(filterPositive([-1, 0, 5, -10, 56])); // => [-1, -10]
+console.log(filterPositive([-25, 25, 0, -1000, -2])); // => [-25, -1000, -2]
